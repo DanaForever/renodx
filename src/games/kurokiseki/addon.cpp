cc2ec0de3e5953ffaf50e47d2a9e07b96cbee820
DanaForever <danaiclucia2020@gmail.com>
2025-12-13 17:48:48 +0700

    Removed UI brightness (resorting to the game's slider) in Kuro Kiseki

--- src/games/kurokiseki/addon.cpp
+++ src/games/kurokiseki/addon.cpp
@@ -232,6 +232,7 @@ renodx::utils::settings::Settings settings = {
         .tooltip = "Sets the brightness of UI and HUD elements in nits",
         .min = 48.f,
         .max = 500.f,
+        .is_visible = []() { return false; },
     },
     new renodx::utils::settings::Setting{
         .key = "InverseToneMapExtraHDRSaturation",
