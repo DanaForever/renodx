e9badda6afbc2db9a9bfc851a7132349a3ed496b
DanaForever <danaiclucia2020@gmail.com>
2026-04-12 22:02:10 +0700

    Persona 5 Royal: fix clipping transitions

--- src/games/p5r/0x_uitransition_0xCC71BBE3.ps_5_0.hlsl
+++ src/games/p5r/0x_uitransition_0xCC71BBE3.ps_5_0.hlsl
@@ -9,7 +9,9 @@ float4 main(float4 v0 : SV_POSITION0, float4 v1 : COLOR0, float2 v2 : TEXCOORD0)
 
   float4 o0 = diffuseTexture.SampleLevel(diffuseSampler_s, v2.xy, 1) * v1;
   if (injectedData.clampState == CLAMP_STATE__OUTPUT) {
-    o0 = saturate(o0);
+    // o0 = saturate(o0);
+    o0.w = saturate(o0.w);
+    o0.rgb = max(0.f, o0.rgb);
   }
   return o0;
 }
